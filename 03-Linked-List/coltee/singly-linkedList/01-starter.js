c0fffc4e001454f8b619db70d3b6f8c451afd01e
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2022-09-18 23:33:00 +0600

    practice Linked list

---
+++ 03-Linked-List/coltee/singly-linkedList/01-starter.js
@@ -0,0 +1,27 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class singlyLinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
